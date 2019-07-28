e8033640890c1bce96311f727771ad66a097f262
Snorre Ulvin Garberg <snorre.ulvin@gmail.com>
2019-07-28 22:29:30 +0200

    Fixed dmg issue

--- src/components/NewEncounter/utility/getMonsterByLevel.tsx
+++ src/components/NewEncounter/utility/getMonsterByLevel.tsx
@@ -2,11 +2,11 @@ import {default as monstersByLevel} from './monsterByLevel.json';
 import {IMonster} from "../types/monster";
 
 const getMonsterByLevel = (level: string): IMonster => {
-  const monster = (monstersByLevel as IMonster[]).find(item => item.level === level);
+  const monster: IMonster | undefined = monstersByLevel.find(item => item.level === level);
   if (monster) {
     return monster;
   }
-  return monstersByLevel[0] as IMonster;
+  return monstersByLevel[0];
 };
 
 export default getMonsterByLevel;
