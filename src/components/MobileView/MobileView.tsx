3d314104007881b77c025091165be1bb9abbb6f4
Snorre Ulvin Garberg <snorre.ulvin@gmail.com>
2019-07-30 11:15:15 +0200

    Improved mobile layout

--- src/components/MobileView/MobileView.tsx
+++ src/components/MobileView/MobileView.tsx
@@ -62,10 +62,8 @@ function MobileView({}: IProps) {
   return (
     <Sidebar
       sidebar={( activeItem && (
-        <div className="pb-5">
-          <div className="p-2 pb-5">
-            <ActiveEncounter encounter={activeItem}/>
-          </div>
+        <div className="p-2 pb-5">
+          <ActiveEncounter encounter={activeItem}/>
         </div>
       ))}
       open={!!activeItem}
@@ -73,40 +71,44 @@ function MobileView({}: IProps) {
       styles={{ sidebar: { background: "white", padding: "5px", maxWidth: '75vw', width: '100%', zIndex: '1040' } }}
       pullRight
     >
-      <div className="pb-5">
-        <EncounterList />
-      </div>
-      {bottomBarView === 'new' && (
-        <BottomBar onClickOutside={handleChangeView('')}>
-          <NewEncounter />
-        </BottomBar>
-      )}
-      {bottomBarView === 'deal' && (
-        <div className="position-fixed bg-light p-3" style={{width: '100vw', height: '175px', bottom: '0', left: '0', zIndex: 1040}}>
-          <div className="d-flex justify-content-center">
-            <HealthSelector value={damageMod} onChangeHp={handleHealthSelection} />
-          </div>
-          <Button onClick={handleChangeView('')} color="danger" size="lg" className="rounded-circle position-fixed" style={{bottom: '15px', right: '15px'}}>
-            X
-          </Button>
+      <div className="d-flex flex-column" style={{ height: '100vh' }}>
+        <div className="pb-5 h-100 overflow-auto">
+          <EncounterList />
         </div>
-      )}
-      <div className="fixed-bottom">
-        <ButtonGroup className="d-flex">
-          <Button className="w-100 c-mobile-nav__item" onClick={handleRoll} color="info" size="lg">
-            Roll
-          </Button>
-          <Button className="w-100 c-mobile-nav__item" onClick={handleClearList} color="danger" size="lg">
-            Clear
-          </Button>
-          <Button className="w-100 c-mobile-nav__item" onClick={handleChangeView('deal')} color="warning" size="lg">
-            Deal
-          </Button>
-          <Button className="w-100 c-mobile-nav__item" onClick={handleChangeView('new')} color="success" size="lg">
-            New
-          </Button>
-        </ButtonGroup>
+        {bottomBarView === 'deal' ? (
+          <div className="bg-light p-3" style={{width: '100vw', height: '175px'}}>
+            <div className="d-flex justify-content-center">
+              <HealthSelector value={damageMod} onChangeHp={handleHealthSelection} />
+            </div>
+            <Button onClick={handleChangeView('')} color="danger" size="lg" className="rounded-circle position-fixed" style={{bottom: '15px', right: '15px'}}>
+              X
+            </Button>
+          </div>
+        ) : (
+          <div>
+            <ButtonGroup className="d-flex">
+              <Button className="w-100 c-mobile-nav__item" onClick={handleRoll} color="info" size="lg">
+                Roll
+              </Button>
+              <Button className="w-100 c-mobile-nav__item" onClick={handleClearList} color="danger" size="lg">
+                Clear
+              </Button>
+              <Button className="w-100 c-mobile-nav__item" onClick={handleChangeView('deal')} color="warning" size="lg">
+                Deal
+              </Button>
+              <Button className="w-100 c-mobile-nav__item" onClick={handleChangeView('new')} color="success" size="lg">
+                New
+              </Button>
+            </ButtonGroup>
+          </div>
+        )}
+        {bottomBarView === 'new' && (
+          <BottomBar onClickOutside={handleChangeView('')}>
+            <NewEncounter />
+          </BottomBar>
+        )}
       </div>
+
     </Sidebar>
   );
 }
